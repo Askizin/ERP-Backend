38773767e13c20cba3fb348aa730fd7b29f0e171
Iury Barbosa <iury_barbosa07@hotmail.com>
2021-11-02 11:39:55 -0300

    New structure

---
+++ src/modules/permissions/model/Permission.ts
@@ -0,0 +1,13 @@
+import {Column, Entity} from "typeorm";
+import {BaseEntity} from "../../base/model/BaseEntity";
+
+@Entity("permissions")
+class Permission extends BaseEntity {
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+}
+
+export {Permission}
